12af557a581756daf60fdc649a597b95404209f3
iptime-fox <dudndi826@gmail.com>
2023-08-17 16:45:28 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Details from './pages/Details';
+import Home from './pages/Home';
+import SearchLists from './pages/SearchLists';
+
+function App() {
+  return (
+    <div>
+      <Home />
+      <SearchLists />
+      <Details />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/SearchLists.js
@@ -0,0 +1,19 @@
+import React, { useEffect } from 'react';
+import { fetchData, getOptions } from '../utils/fetchData';
+
+const SearchLists = () => {
+  useEffect(() => {
+    const getSearchData = async () => {
+      const getData = await fetchData(
+        'https://airbnb13.p.rapidapi.com/search-location?location=Paris&checkin=2023-09-16&checkout=2023-09-17&adults=1&children=0&infants=0&pets=0&page=1&currency=USD',
+        getOptions
+      );
+      console.log(getData);
+    };
+    // getSearchData();
+  }, []);
+
+  return <div>SearchLists</div>;
+};
+
+export default SearchLists;
