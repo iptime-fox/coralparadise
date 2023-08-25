4bdc016883e5323a73d51c06e42dfc05156c65a7
iptime-fox <dudndi826@gmail.com>
2023-08-25 20:25:02 +0900

    오타 수정 0825

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,7 @@ import React, { useEffect, useRef, useState } from 'react';
 import Header from '../components/Header';
 import { Helmet } from 'react-helmet';
 import Landing from '../components/Landing';
-import CustomSearch from '../components/CustonSearch';
+import CustomSearch from '../components/CustomSearch';
 import SliderSection from '../components/SliderSection';
 
 const Home = () => {
