9097f4bed7757babe369b07353457cfd64ca4e60
iptime-fox <dudndi826@gmail.com>
2023-09-24 22:30:59 +0900

    검색 날짜 수정 0924

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -36,7 +36,7 @@ const SearchBoxes = () => {
   useEffect(() => {
     const getSearchData = async () => {
       const getData = await fetchData(
-        'https://airbnb13.p.rapidapi.com/search-location?location=Paris&checkin=2023-09-16&checkout=2023-09-17&adults=1&children=0&infants=0&pets=0&page=1&currency=USD',
+        'https://airbnb13.p.rapidapi.com/search-location?location=Paris&checkin=2023-11-16&checkout=2023-11-17&adults=1&children=0&infants=0&pets=0&page=1&currency=USD',
         getOptions
       );
       setMain(getData.results[0]);
